91e430afaab9a718cd828b8d719d4908894e3bf2
Ricardo Inostoza Rebolledo <rinostrozareb@gmail.com>
2021-03-02 23:55:23 -0300

    coors2

--- get_token.php
+++ get_token.php
@@ -1,7 +1,9 @@
 <?php
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: GET, POST');
-header("Access-Control-Allow-Headers: X-Requested-With");
+ header('Access-Control-Allow-Origin: *'); 
+ header("Access-Control-Allow-Credentials: true");
+ header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
+ header('Access-Control-Max-Age: 1000');
+ header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token , Authorization');
 
 $curl = curl_init();
 
