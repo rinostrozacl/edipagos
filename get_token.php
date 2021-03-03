4fcfbd7210de37c66c3927eeb797071e906ebfe6
Ricardo Inostoza Rebolledo <rinostrozareb@gmail.com>
2021-03-03 01:23:31 -0300

    fds

--- get_token.php
+++ get_token.php
@@ -1,5 +1,4 @@
 <?php
-header('Access-Control-Allow-Origin: *');
 header("Access-Control-Allow-Credentials: true");
 header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
 header('Access-Control-Max-Age: 1000');
